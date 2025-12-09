d0e4d35106fdd4070652cd6b3159bbb00f473193
louisdev12 <221082834+louisdev12@users.noreply.github.com>
2025-12-09 23:49:26 +0300

    feat: updated app structure

--- Tradelio/src/main.jsx
+++ Tradelio/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Add this line
+import "./index.css";
+
+// Remove the placeholder as soon as JS loads
+document.getElementById("startup")?.remove();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
