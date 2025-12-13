1ee57082372941ecfc75d19e9024093c8cbe4d42
louisdev12 <221082834+louisdev12@users.noreply.github.com>
2025-12-13 15:26:05 +0300

    feat: updated library code reuse

--- Tradelio/src-tauri/src/lib.rs
+++ Tradelio/src-tauri/src/lib.rs
@@ -1,4 +1,7 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod passcode;
+mod profile;
 
 use passcode::PasscodeState;
 
@@ -7,8 +10,10 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_dialog::init())  // ADD THIS LINE
         .manage(PasscodeState::new())
         .invoke_handler(tauri::generate_handler![
+            // Passcode
             passcode::check_passcode_exists,
             passcode::setup_passcode,
             passcode::unlock_app,
@@ -17,6 +22,10 @@ pub fn run() {
             passcode::get_lock_settings,
             passcode::update_lock_settings,
             passcode::disable_passcode,
+
+            // Profile
+            profile::save_profile_image,
+            profile::get_profile
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
